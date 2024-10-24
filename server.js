b1257a34c91b5e80ac13e0322b156842c84edfe1
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-24 11:09:20 +0800

    混合使用普通和路径/方法限制的中间件

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const app = express();
-// app.get(路径, 中间件1, 中间件2, 中间件3)
+// 1.注册两个普通的中间件
+app.use((req, res, next) => {
+ console.log('normal middleware01');
+ next();
+});
+app.use((req, res, next) => {
+ console.log('normal middleware02');
+ next();
+});
+// 2.注册路径path/method的中间件
 app.get('/home',
  (req, res, next) => {
- console.log('match /home get middleware01');
+ console.log('/home get middleware01');
  next();
  },
  (req, res, next) => {
- console.log('match /home get middleware02');
- next();
- },
- (req, res, next) => {
- console.log('match /home get middleware03');
- next();
- },
- (req, res, next) => {
- console.log('match /home get middleware04');
- res.end('All middleware executed');
+ console.log('/home get middleware02');
+ res.end('GET /home response completed');
  }
 );
+app.post('/login', (req, res, next) => {
+ console.log('/login post middleware');
+ res.end('POST /login response completed');
+});
+// 3.注册普通的中间件
+app.use((req, res, next) => {
+ console.log('normal middleware03');
+ next();
+});
+app.use((req, res, next) => {
+ console.log('normal middleware04');
+});
 app.listen(9000, () => {
  console.log('Express服务器启动成功~');
 });
