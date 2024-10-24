99a771b8b5bbd7d6e8d7262601c744271d5d8d65
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-24 11:47:34 +0800

    使用Express内置JSON中间件

--- server.js
+++ server.js
@@ -1,39 +1,29 @@
 const express = require('express');
 const app = express();
+// 使⽤Express内置的JSON中间件解析JSON数据
+app.use(express.json());
+// 注册两个实际请求的中间件
 app.post('/login', (req, res, next) => {
- let isLogin = false;
- req.on('data', (data) => {
- const dataString = data.toString();
- const dataInfo = JSON.parse(dataString);
- if (dataInfo.username === 'gkd' && dataInfo.password=== '123456') {
- isLogin = true;
- }
- });
- req.on('end', () => {
- if (isLogin) {
+ // 通过Express内置的中间件，req.body已经拥有解析后的JSON数据
+ const { username, password } = req.body;
+ console.log(req.body);
+ if (username === 'gkd' && password === '123456') {
  res.end('登录成功, 欢迎回来~');
  } else {
  res.end('登录失败, 请检测账号和密码是否正确~');
  }
- });
 });
 // 案例⼆: 注册⽤户的请求处理 /register post => username/password
 app.post('/register', (req, res, next) => {
- // 1.获取本次请求过程中传递过来的json数据
- let isRegister = false;
- req.on('data', (data) => {
- const dataString = data.toString();
- const dataInfo = JSON.parse(dataString);
- // 查询数据库中该⽤户是否已经注册过
- isRegister = false;
- });
- req.on('end', () => {
- if (isRegister) {
- res.end('注册成功, 开始你的旅程~');
- } else {
+ const { username } = req.body;
+ console.log(req.body);
+ // 模拟现有⽤户查询逻辑
+ const existingUsers = ['existinguser'];
+ if (existingUsers.includes(username)) {
  res.end('注册失败, 您输⼊的⽤户名被注册~');
+ } else {
+ res.end('注册成功, 开始你的旅程~');
  }
- });
 });
 app.listen(9000, () => {
  console.log('Express服务器启动成功~');
