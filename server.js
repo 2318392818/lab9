6fef4eef0b9596f5e7f90aa2d1724ac737a7e73f
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-24 10:59:36 +0800

    添加路径匹配的中间件已处理/home请求

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const app = express();
-// 总结: 当express接收到客户端发送的⽹络请求时, 在所有中间件中开始进⾏匹配
-// 当匹配到第⼀个符合要求的中间件时, 那么就会执⾏这个中间件
-// 后续的中间件是否会执⾏呢? 取决于上⼀个中间件有没有执⾏next
-// 通过use⽅法注册的中间件是最普通的/简单的中间件
-// 通过use注册的中间件, ⽆论是什么请求⽅式都可以匹配上
-// login/get, login/post, abc/patch等
-app.use((req, res, next) => {
- console.log('normal middleware 01');
- // 例如： res.end('返回结果了, 不要等了'); // 若取消注释，将阻⽌后续中间件执⾏
- next();
+// 注册普通的中间件
+// app.use((req, res, next) => {
+// console.log('match normal middleware');
+// res.end('--------');
+// });
+// 注册路径匹配的中间件
+// 路径匹配的中间件是不会对请求⽅式(method)进⾏限制
+app.use('/home', (req, res, next) => {
+ console.log('match /home middleware');
+ res.end('home data');
 });
-app.use((req, res, next) => {
- console.log('normal middleware 02');
-});
-// 开启服务器
 app.listen(9000, () => {
  console.log('Express服务器启动成功~');
 });
