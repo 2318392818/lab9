8b264bbd86da6d91bdf9a29641b33ea28efd6138
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-24 10:55:08 +0800

    在Expresss添加中间件功能

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
-// 客户端访问URL: /login和/home
-app.post('/login', (req, res) => {
- // 处理login请求
- res.end('登录成功, 欢迎回来~');
+// 给express创建的app传⼊⼀个回调函数
+// 传⼊的这个回调函数就称之为是中间件(middleware)
+// app.post('/login', 回调函数 => 中间件)
+app.post('/login', (req, res, next) => {
+ // 1.中间件中可以执⾏任意代码
+ console.log('first middleware exec~');
+ // 其他操作，如查询数据、判断逻辑等等
+ // 2.在中间件中修改req/res对象
+ req.age = 99;
+ // 3.可以在中间件中结束响应周期
+ // res.json({ message: "登录成功, 欢迎回来", code: 0 });
+ // 4.执⾏下⼀个中间件
+ next();
 });
-app.get('/home', (req, res) => {
- res.end('⾸⻚的轮播图/推荐数据列表~');
+app.use((req, res, next) => {
+ console.log('second middleware exec~');
 });
-// 2.启动服务器, 并且监听端⼝
 app.listen(9000, () => {
  console.log('Express服务器启动成功~');
 });
