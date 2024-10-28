5293ec787468acde1d9eed9832800c2c10f530fe
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-28 10:50:21 +0800

    使用urlencoded

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
 const express = require('express');
+// 创建app对象
 const app = express();
-// 使⽤Express内置的JSON中间件解析JSON数据
-app.use(express.json());
-// 注册两个实际请求的中间件
+// 应⽤⼀些中间件
+app.use(express.json()); // 解析客户端传递过来的json
+// 解析传递过来的urlencoded数据, 默认使⽤的是node的内置querystring模块
+// { extended: true }: 表⽰不再使⽤内置的querystring, ⽽是使⽤qs第三⽅库
+app.use(express.urlencoded({ extended: true })); //解析客户端传递过来的urlencoded数据
+// 编写中间件
 app.post('/login', (req, res, next) => {
- // 通过Express内置的中间件，req.body已经拥有解析后的JSON数据
- const { username, password } = req.body;
- console.log(req.body);
- if (username === 'gkd' && password === '123456') {
- res.end('登录成功, 欢迎回来~');
- } else {
- res.end('登录失败, 请检测账号和密码是否正确~');
- }
+console.log(req.body);
+res.end('登录成功, 欢迎回来~');
 });
-// 案例⼆: 注册⽤户的请求处理 /register post => username/password
-app.post('/register', (req, res, next) => {
- const { username } = req.body;
- console.log(req.body);
- // 模拟现有⽤户查询逻辑
- const existingUsers = ['existinguser'];
- if (existingUsers.includes(username)) {
- res.end('注册失败, 您输⼊的⽤户名被注册~');
- } else {
- res.end('注册成功, 开始你的旅程~');
- }
-});
+// 启动服务器
 app.listen(9000, () => {
- console.log('Express服务器启动成功~');
+console.log('express服务器启动成功~');
 });
