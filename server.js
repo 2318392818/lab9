64afcfcb62ce5ad2aa68bdfcf8a70919d990945d
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-28 11:26:21 +0800

    实现文件上传功能

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-const fs = require('fs');
 const express = require('express');
-const morgan = require('morgan');
+const multer = require('multer');
+// 创建app对象
 const app = express();
-const writeStream = fs.createWriteStream('./logs/access.log', { flags: 'a' });
-app.use(morgan('combined', { stream: writeStream }));
-app.post('/login', (req, res, next) => {
-res.end('登录成功, 欢迎回来~');
+// 应⽤⼀个express编写的第三⽅中间件
+// 初始化multer，并指定上传⽂件存放的⽬录
+const upload = multer({ dest: './uploads' });
+// 编写中间件
+// 上传单⽂件: 使⽤multer的single⽅法，处理名为'avatar'的⽂件
+app.post('/avatar', upload.single('avatar'), (req, res, next) => {
+console.log(req.file); // 打印上传的⽂件信息，例如路径、⼤⼩等
+res.end('⽂件上传成功~');
 });
+// 启动服务器
 app.listen(9000, () => {
 console.log('express服务器启动成功~');
 });
