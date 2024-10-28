c30ed7d2a18ad220c01eea50531e92a0c347337c
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-28 11:47:56 +0800

    多文件上传

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ const express = require('express');
 const multer = require('multer');
 // 创建app对象
 const app = express();
-// 应⽤⼀个express编写的第三⽅中间件
-// 初始化multer，并指定上传⽂件存放的⽬录
-const upload = multer({ dest: './uploads' });
-// 编写中间件
-// 上传单⽂件: 使⽤multer的single⽅法，处理名为'avatar'的⽂件
+// 配置Multer的存储选项，使⽤⾃定义的diskStorage引擎
+const upload = multer({
+storage: multer.diskStorage({
+destination(req, file, callback) {
+// 指定⽂件存放⽬录
+callback(null, './uploads');
+},
+filename(req, file, callback) {
+// 定义⽂件保存时的⽂件名, 使⽤当前时间戳+原始⽂件名
+callback(null, Date.now() + '_' + file.originalname);
+},
+}),
+});
+// 单⽂件上传
 app.post('/avatar', upload.single('avatar'), (req, res, next) => {
-console.log(req.file); // 打印上传的⽂件信息，例如路径、⼤⼩等
+console.log(req.file); // 打印单个上传⽂件的信息Lab10 Express的初步使⽤2 10
 res.end('⽂件上传成功~');
+});
+// 多⽂件上传
+app.post('/photos', upload.array('photos'), (req, res, next) => {
+console.log(req.files); // 打印多个上传⽂件的信息
+res.end('上传多张照⽚成功~');
 });
 // 启动服务器
 app.listen(9000, () => {
