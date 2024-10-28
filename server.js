388461e281b6fd216f97c0d04d36121ae337164d
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-28 11:11:50 +0800

    添加moragn中间件

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
+const fs = require('fs');
 const express = require('express');
-// 创建app对象
+const morgan = require('morgan');
 const app = express();
-// 应⽤⼀些中间件
-app.use(express.json()); // 解析客户端传递过来的json
-// 解析传递过来的urlencoded数据, 默认使⽤的是node的内置querystring模块
-// { extended: true }: 表⽰不再使⽤内置的querystring, ⽽是使⽤qs第三⽅库
-app.use(express.urlencoded({ extended: true })); //解析客户端传递过来的urlencoded数据
-// 编写中间件
+const writeStream = fs.createWriteStream('./logs/access.log', { flags: 'a' });
+app.use(morgan('combined', { stream: writeStream }));
 app.post('/login', (req, res, next) => {
-console.log(req.body);
 res.end('登录成功, 欢迎回来~');
 });
-// 启动服务器
 app.listen(9000, () => {
 console.log('express服务器启动成功~');
 });
