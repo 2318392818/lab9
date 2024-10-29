8567eb72160a02bc2a071d0257da74325c62cf60
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-29 16:12:06 +0800

    添加多种响应Experss方法

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 // 创建app对象
 const app = express();
-// 1. 解析queryString
-app.get('/home/list', (req, res, next) => {
-// 从URL中解析offset和size
-const queryInfo = req.query; // 通过req.query获取查询字符串中的参数对象
-console.log(queryInfo);
-res.end('data list数据');
+// 编写中间件⽤于处理/login路由的POST请求
+app.post('/login', (req, res, next) => {
+
+res.end('登录成功, 欢迎回来~');
+// 2. 使⽤res.json⽅法（最常⽤）
+// 返回JSON格式的数据
+/*
+res.json({
+code: 0,
+message: '欢迎回来~',
+list: [
+{ name: 'iPhone', price: 111 },
+{ name: 'iPad', price: 111 },
+{ name: 'iMac', price: 111 },
+{ name: 'Mac', price: 111 },
+]
 });
-// 2. 解析params参数
-app.get('/users/:id', (req, res, next) => {
-const id = req.params.id; // 从req.params中提取路径参数
-res.end(`获取到${id}的数据~`);
+*/
+// 3. 使⽤res.status⽅法设置HTTP状态码，结合res.json
+res.status(201); // 设置状态码为201（资源已创建）
+res.json('创建⽤户成功~'); // 返回信息
 });
 // 启动服务器
 app.listen(9000, () => {
