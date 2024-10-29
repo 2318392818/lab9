99cf294ce8cbf487288ad92fdddaa48c0d31c43a
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-29 15:50:56 +0800

    使用multer处理任意表单数据和请求体解析

--- server.js
+++ server.js
@@ -2,28 +2,15 @@ const express = require('express');
 const multer = require('multer');
 // 创建app对象
 const app = express();
-// 配置Multer的存储选项，使⽤⾃定义的diskStorage引擎
-const upload = multer({
-storage: multer.diskStorage({
-destination(req, file, callback) {
-// 指定⽂件存放⽬录
-callback(null, './uploads');
-},
-filename(req, file, callback) {
-// 定义⽂件保存时的⽂件名, 使⽤当前时间戳+原始⽂件名
-callback(null, Date.now() + '_' + file.originalname);
-},
-}),
-});
-// 单⽂件上传
-app.post('/avatar', upload.single('avatar'), (req, res, next) => {
-console.log(req.file); // 打印单个上传⽂件的信息Lab10 Express的初步使⽤2 10
-res.end('⽂件上传成功~');
-});
-// 多⽂件上传
-app.post('/photos', upload.array('photos'), (req, res, next) => {
-console.log(req.files); // 打印多个上传⽂件的信息
-res.end('上传多张照⽚成功~');
+// 使⽤Express内置的中间件解析JSON和URL编码数据
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 设置Multer实例
+const formdata = multer();
+// 编写处理请求体的中间件，使⽤formdata.any()处理任意表单请求数据
+app.post('/login', formdata.any(), (req, res, next) => {
+console.log(req.body); // 打印请求体中解析的普通数据
+res.end('登录成功, 欢迎回来~');
 });
 // 启动服务器
 app.listen(9000, () => {
