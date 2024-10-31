8cf351ade0f22296134cdc7f166a14d78238b22d
宋亚轩超绝干拉 <14968262+song-yaxuan-is-super-dry@user.noreply.gitee.com>
2024-10-31 09:19:58 +0800

    配置静态资源管理

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const userRouter = require('./userRouter');
 // 创建app对象
 const app = express();
-// 编写中间件
+// 使⽤内置的中间件将⽂件夹做为静态资源
+// 此设置使得`uploads`⽬录和`build`⽬录中的⽂件可以被客户端直接访问
+app.use(express.static('./uploads'));
+app.use(express.static('./build'));
+// ⽰例路由中间件
 app.post('/login', (req, res, next) => {
-// 登录逻辑
+ // 登录逻辑
 });
-app.get('/home', (req, res, next) => {
-// ⾸⻚逻辑
-});
-// 使⽤⽤户路由器
-app.use('/users', userRouter);
 // 启动服务器
 app.listen(9000, () => {
-console.log('express服务器启动成功~');
+ console.log('express服务器启动成功~');
 });
